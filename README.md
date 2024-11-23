bce322f48558fea789ac022798d257ba3abcd899
acshameless <acshameless@gmail.com>
2024-11-23 10:51:30 +0800

    feat: init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Read Notes
+
+记录工程师 shameless 的 阅读记录，不定期发布，欢迎关注，本 repo Fork 自工程师 Tw93 https://github.com/tw93/weekly，特此声明。
+
+* [第 00 本 - example](https://weekly.shameless.top/posts/00-example)
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  title: 'Read Notes',
+  author: 'shameless',
+  description: '记录工程师 shameless 的 read notes，不定期发布，欢迎关注',
+  keywords: 'ac,shameless,Blog,AWS,read notes',
+  icon: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  pic: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  homePage: 'https://read.shameless.top',
+  blogPage: 'https://ac.shameless.top/',
+  twitterId: 'acshameless',
+  githubId: 'acshameless',
+  repo: 'acshameless/read-website',
+};
